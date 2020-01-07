7d16ef5590e980c9528392bd84364697f6d39008
ArunKumar <arunkumar.sugumar@asg.com>
2020-01-07 12:20:31 +0530

    -- Routing -- Observables

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
   constructor(private shoppingListRecipe: ShoppingListService) {
   }
 
   ngOnInit() {
     this.ingredients = this.shoppingListRecipe.getIngredients();
-    this.shoppingListRecipe.ingredientsChanged
+    this.igChangeSub = this.shoppingListRecipe.ingredientsChanged
       .subscribe((ingredient: Ingredient[]) => this.ingredients = ingredient);
+  }
+
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
   }
 
 }
